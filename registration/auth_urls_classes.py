32d90df6fa9d08cac1b180e99eeb356913f217fb
Victor <zhuyuecai@gmail.com>
2018-01-13 21:02:15 -0500

    reverse lazy with view function

--- registration/auth_urls_classes.py
+++ registration/auth_urls_classes.py
@@ -34,28 +34,28 @@ urlpatterns = [
         name='auth_logout'),
     url(r'^password/change/$',
         auth_views.PasswordChangeView.as_view(
-            success_url='auth_password_change_done')
+            success_url=reverse_lazy(auth_views.PasswordChangeDoneView.as_view()))
         ),
         name='auth_password_change'),
-    url(r'^password/change/auth_password_change_done/$',
+    url(r'^password/change/done/$',
         auth_views.PasswordChangeDoneView.as_view(),
         name='auth_password_change_done'),
     url(r'^password/reset/$',
         auth_views.PasswordResetView.as_view(
             email_template_name='registration/password_reset_email.txt',
-            success_url='auth_password_reset_done',
+            success_url=reverse_lazy(auth_views.PasswordResetDoneView.as_view()),
         ),
         name='auth_password_reset'),
-    url(r'^password/reset/auth_password_reset_complete/$',
+    url(r'^password/reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(),
         name='auth_password_reset_complete'),
-    url(r'^password/reset/auth_password_reset_done/$',
+    url(r'^password/reset/done/$',
         auth_views.PasswordResetDoneView.as_view(),
         name='auth_password_reset_done'),
     url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/'
         r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(
-            success_url='auth_password_reset_complete'
+            success_url=reverse_lazy(auth_views.PasswordResetCompleteView.as_view())
         ),
         name='auth_password_reset_confirm'),
 ]
